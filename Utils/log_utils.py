ac497ff3368de7a03024e86ed75dc2e182b34a67
Rajamegam <rajamegam7@gmail.com>
2024-11-28 18:03:05 +0530

    AWS changes

--- Utils/log_utils.py
+++ Utils/log_utils.py
@@ -1,17 +1,14 @@
 import logging
 import os
 from datetime import datetime
-
 import boto3
-
-from Utilities.configurations import config
 
 
 class logUtils:
     def __init__(self):
         self.S3_BUCKET_NAME = "pytestautomationlogs"
-        self.LOG_FILE_NAME = f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-        self.LOCAL_LOG_FILE_PATH = f"./logs/{self.LOG_FILE_NAME}"
+        self.LOG_FILE_NAME = "logfile_2024-11-28_17-14-42.log"
+        self.LOCAL_LOG_FILE_PATH = "D:/API automation/Restful_Booker_Automation/logs/logfile_2024-11-28_17-14-42.log"
 
     @staticmethod
     def get_logger():
@@ -30,12 +27,12 @@ class logUtils:
     def upload_logs_to_s3(self):
         client = boto3.client(
             "s3",
-            aws_access_key_id=config()["AWS credentials"]["AWS_ACCESS_KEY_ID"],
-            aws_secret_access_key=config()["AWS credentials"]["AWS_SECRET_ACCESS_KEY"],
-            region_name=os.getenv("AWS_REGION", "us-east-1"),
+            aws_access_key_id="",
+            aws_secret_access_key="",
+            region_name="us-east-1"
         )
         try:
-            client.upload_file(self.LOCAL_LOG_FILE_PATH, self.S3_BUCKET_NAME, self.LOG_FILE_NAME, )
+            client.upload_file(self.LOCAL_LOG_FILE_PATH, self.S3_BUCKET_NAME)
             print(f"Log file uploaded successfully")
         except Exception as e:
             print(f"Failed to upload log file: {e}")
